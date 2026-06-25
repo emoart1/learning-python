0cb031a951b6a08a4ef84fb05a64891ba2a21230
emoart1 <emoart@proton.me>
2026-06-25 21:14:19 +0200

    start to lists and loops
    starting to work on loops and lists
    not done yet, will continue later (probably tomorrow)

---
+++ lists-and-dictionaries.py
@@ -0,0 +1,9 @@
+# lists and dictionaries in python 
+# lists are used to store multiple items in a single variable 
+# to create a list use [] insead of () when assining a variable 
+# when creating a list seperate the data/text/value with a comma (,) 
+
+alist = [1, 2, 3, 4, 5] # list of integers (numeric) 
+ThisList = ["apple", "banana", "cherry", "apple", "cherry", "strawberry"] # a list of sttings, often called a string 
+that_list = [True, False, False, True, True, False] # a list of boolean values 
+
